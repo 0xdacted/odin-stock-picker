334c7e23abd7b4912b920fd95d4d88b0edea8117
Red <crabmarket56@gmail.com>
2022-07-13 13:09:20 -0400

    Clean formatting

--- script.rb
+++ script.rb
@@ -1,9 +1,5 @@
-# Implement a method #stock_picker that takes in an array of stock prices, one for each hypothetical day. 
-# It should return a pair of days representing the best day to buy and the best day to sell. Days start at 0.
-
 # takes input array of integers. For each integer, check the value of every integer with an index larger than it - original integer. 
 # returns index positions of values that create largest possible value representing second integer - first integer
-require 'pry-byebug'
 def stock_picker(array)
 profit_array = []
 
@@ -15,11 +11,9 @@ profit_array = []
       index += 1
       profit = array[index].to_i - current_price
       profit_array.push(profit: profit, index1: original_index, index2: index)
-    
     end
 end
  sorted = profit_array.sort_by {|profit| [profit[:profit]]}
-
-p sorted[-1]
+ p sorted[-1]
 end
 stock_picker([17,3,6,9,15,8,6,1,10]) 
